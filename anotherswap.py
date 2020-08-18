d6d96260609d8a98d0f98d1562ea7684fc7fcd6f
Kevin Thomas <kevin.thomas2634@gmail.com>
2020-08-18 14:46:32 -0700

    Initial commit

---
+++ anotherswap.py
@@ -0,0 +1,10 @@
+x = 111
+y = 222
+print("Before swap x is:" + str(x) )
+print("Before swap y is:" + str(y) )
+x = x + y
+y = x - y
+x = x - y
+
+print("After swap x is:" + str(x))
+print("After swap y is:" + str(y))
